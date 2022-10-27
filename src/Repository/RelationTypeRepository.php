2277d97bbf2b637002ba73eb6c5f2b7e3b054a04
Axel Vion <axel.vionyt@gmail.com>
2022-10-27 17:59:15 +0200

    Add entity RelationType

---
+++ src/Repository/RelationTypeRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\RelationType;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<RelationType>
+ *
+ * @method RelationType|null find($id, $lockMode = null, $lockVersion = null)
+ * @method RelationType|null findOneBy(array $criteria, array $orderBy = null)
+ * @method RelationType[]    findAll()
+ * @method RelationType[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RelationTypeRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, RelationType::class);
+    }
+
+    public function save(RelationType $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(RelationType $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return RelationType[] Returns an array of RelationType objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('r.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?RelationType
+//    {
+//        return $this->createQueryBuilder('r')
+//            ->andWhere('r.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
