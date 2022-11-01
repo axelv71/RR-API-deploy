bdfaa1fba4e21b7eb6bae553e62bdd5ea633b001
RomainCrz <rom.c001@icloud.com>
2022-11-01 09:59:15 +0100

    Created Ressource Controller

---
+++ src/Controller/RessourceController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RessourceController extends AbstractController
+{
+
+}
