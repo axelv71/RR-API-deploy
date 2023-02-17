c967c90cbfa934b1f3e9c5079984d5c32ca66fec
RomainCrz <rom.c001@icloud.com>
2023-02-18 00:06:33 +0100

    added refresh token route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -51,4 +51,36 @@ class LoginController extends AbstractController
     public function userLogin(Request $request,
                               UserRepository $userRepository)
     {}
+
+
+    #[OA\Tag(name: "Auth")]
+    #[OA\RequestBody(
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(
+                    property: "refresh_token",
+                    type: "string",
+                    example:"xxx00a7a9e970f9bbe076e05743e00648908c38366c551a8cdf524ba424fc3e520988f6320a549e5"
+                )
+            ]
+        )
+    )]
+    #[OA\Response(
+        response: 201,
+        description: "Returns a JWT token",
+        content: new OA\JsonContent(
+            type: "object",
+            properties: [
+                new OA\Property(
+                    property: "token",
+                    type: "string",
+                    example: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE2NzYzMjM1MzQsImV4...."
+                )
+            ]
+        )
+    )]
+    #[Route('/api/token/refresh', name: 'app_refresh_token', methods: ['POST'])]
+    public function refreshToken(Request $request)
+    {}
 }
