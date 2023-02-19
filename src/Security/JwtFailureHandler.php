ca1af460c16197879552ab4317a5a93e68c0110e
RomainCrz <rom.c001@icloud.com>
2023-02-19 12:01:04 +0100

    changed the way that the refresh token is send when user login

---
+++ src/Security/JwtFailureHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Security;
+
+use App\Repository\UserRepository;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+
+class JwtFailureHandler implements AuthenticationFailureHandlerInterface
+{
+    private $jwtEncoder;
+    private $userRepository;
+
+    public function __construct(JWTEncoderInterface $jwtEncoder, UserRepository $userRepository)
+    {
+        $this->jwtEncoder = $jwtEncoder;
+        $this->userRepository = $userRepository;
+    }
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $user = $this->userRepository->findOneBy(['email' => $data['username']]);
+        if (!$user) {
+            throw new AuthenticationException('Email or password is incorrect');
+        }
+        if (!password_verify($data['password'], $user->getPassword())) {
+            throw new AuthenticationException('Email or password is incorrect');
+        }
+
+        if (!$user->isIsActive()) {
+            throw new AuthenticationException('Your account is not active');
+        }
+
+        if (!$user->isVerified()){
+            throw new AuthenticationException('Your account is not verified');
+        }
+        // Génération du token JWT
+        $payload = [
+            'username' => $user->getUsername(),
+            // Ajoutez toutes les données que vous souhaitez inclure dans le payload ici
+        ];
+        $jwt = $this->jwtEncoder->encode($payload);
+
+        // Renvoi du token JWT
+        return new Response($jwt, Response::HTTP_OK);
+    }
+}
--- src/Security/JwtSuccessHandler.php
+++ src/Security/JwtSuccessHandler.php
@@ -2,26 +2,39 @@
 
 namespace App\Security;
 
+
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
 
-class JwtSuccessHandler implements AuthenticationFailureHandlerInterface
+class JwtSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
     private $jwtEncoder;
     private $userRepository;
+    private $refreshTokenGenerator;
+    private $em;
 
-    public function __construct(JWTEncoderInterface $jwtEncoder, UserRepository $userRepository)
+
+    public function __construct(JWTEncoderInterface $jwtEncoder,
+                                RefreshTokenGeneratorInterface $refreshTokenGenerator,
+                                EntityManagerInterface $em,
+                                UserRepository $userRepository)
     {
         $this->jwtEncoder = $jwtEncoder;
         $this->userRepository = $userRepository;
+        $this->refreshTokenGenerator = $refreshTokenGenerator;
+        $this->em = $em;
+
+
     }
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token) : Response
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token) : JsonResponse
     {
         $data = json_decode($request->getContent(), true);
         $user = $this->userRepository->findOneBy(['email' => $data['username']]);
@@ -45,38 +58,24 @@ class JwtSuccessHandler implements AuthenticationFailureHandlerInterface
             'username' => $user->getUsername(),
             // Ajoutez toutes les données que vous souhaitez inclure dans le payload ici
         ];
+
         $jwt = $this->jwtEncoder->encode($payload);
+        $refresh_token = $this->refreshTokenGenerator->createForUserWithTtl($user, 2592000);
+        $refresh_token->setUsername($user->getUsername());
+        $refresh_token->setValid(\DateTime::createFromFormat('U', time() + 2592000));
+        $this->em->persist($refresh_token);
+        $this->em->flush();
 
-        // Renvoi du token JWT
-        return new Response($jwt, Response::HTTP_OK);
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
-    {
-        $data = json_decode($request->getContent(), true);
-        $user = $this->userRepository->findOneBy(['email' => $data['username']]);
-        if (!$user) {
-            throw new AuthenticationException('Email or password is incorrect');
-        }
-        if (!password_verify($data['password'], $user->getPassword())) {
-            throw new AuthenticationException('Email or password is incorrect');
-        }
-
-        if (!$user->isIsActive()) {
-            throw new AuthenticationException('Your account is not active');
-        }
-
-        if (!$user->isVerified()){
-            throw new AuthenticationException('Your account is not verified');
-        }
-        // Génération du token JWT
-        $payload = [
-            'username' => $user->getUsername(),
-            // Ajoutez toutes les données que vous souhaitez inclure dans le payload ici
+        $data = [
+            'token' => $jwt,
+            'refresh_token' => $refresh_token->getRefreshToken()
         ];
-        $jwt = $this->jwtEncoder->encode($payload);
+
+        $json_data = json_encode($data);
 
         // Renvoi du token JWT
-        return new Response($jwt, Response::HTTP_OK);
+        return new JsonResponse($json_data, Response::HTTP_OK, [], true);
     }
+
+
 }
