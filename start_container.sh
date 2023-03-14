91f0b6be3d69a206e39841dfe788a8561937ea7b
RomainCrz <rom.c001@icloud.com>
2023-03-14 14:52:11 +0100

    changed script

--- start_container.sh
+++ start_container.sh
@@ -4,6 +4,7 @@ CWD=$(pwd)
 
 rm "$CWD"/migrations/*.php
 
+docker exec -ti www_cube /bin/bash -c "composer install"
 docker exec -ti www_cube /bin/bash -c "php bin/console doctrine:database:drop --if-exists --force"
 docker exec -ti www_cube /bin/bash -c "php bin/console doctrine:database:create --if-not-exists"
 docker exec -ti www_cube /bin/bash -c "php bin/console make:migration"
