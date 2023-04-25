dd7d596ad81085f56f6ce547fd7d6b18ff99dbda
Axel Vion <axel.vionyt@gmail.com>
2023-04-25 08:41:31 +0200

    test stats

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
